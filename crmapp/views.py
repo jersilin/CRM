d668abda6087dad1402a773b10819b515b464b3e
jersilin <jersilinpj1998@gmail.com>
2024-06-08 19:19:32 +0530

    dashboard

--- crmapp/views.py
+++ crmapp/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
-   
-    
-    return render(request, 'home.html')
+   return render(request, 'home.html')
+
+def dashboard(request):
+   return render(request, 'dashboard.html')
